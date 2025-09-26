38098e03675ec502e8fcfea96fdaacdbc3b0e3af
tomoyuki tsugawa <schwarz_kaiser@hotmail.co.jp>
2025-09-27 06:10:56 +0900

    公開用に修正

--- catdogfox.py
+++ catdogfox.py
@@ -57,8 +57,8 @@ def upload_file():
     return render_template("index.html", answer="", upload_image="")
 
 
-if __name__ == "__main__":
-    app.run()
 # if __name__ == "__main__":
-#     port = int(os.environ.get('PORT', 8080))
-#     app.run(host ='0.0.0.0',port = port)
+#     app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8080))
+    app.run(host ='0.0.0.0',port = port)
